f970067ccf1bb0c25af64108e6dfc4ddafa60205
SebaEG07 <100600291+SebaEG07@users.noreply.github.com>
2023-03-07 13:51:42 -0300

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,7 @@
+<script>
+    const $form = document.querySelector('#form');
+    $form.addEventListener('sumbit',handleSubmit);
+    function handleSubmit(event){
+        event.preventDefault();
+    }
+</script>
